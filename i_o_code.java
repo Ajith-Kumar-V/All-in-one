5dfea7a48aab02b85ddd838e3e463985e5313f90
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2022-10-11 15:16:16 +0530

    Add files via upload
    input, output code in JAVA

---
+++ i_o_code.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class i_o_code {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a number");
+        String number = obj.next();
+        System.out.println(number);
+
+    }
+}
